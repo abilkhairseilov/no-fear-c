b3f81b8c52ffbb3745cdda1c4a483a59f96b0b4b
bulletzenith <hexagon.tr0@gmail.com>
2024-05-14 11:24:45 +0300

    add: accepting arguments in mortgage

--- mortgage/src/main.c
+++ mortgage/src/main.c
@@ -1,1 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+inline unsigned root(unsigned x, unsigned n)
+{
+    switch (n)
+    {
+    case 0:
+        return 1;
+    case 1:
+        return x;
+    case 2:
+        return (unsigned)sqrt((double)x);
+    case 3:
+        return (unsigned)cbrt((double)x);
+    default:
+        return (unsigned)pow(x, 1.0 / n);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc > 3 | argc < 3)
+    {
+        printf("2 arguments expected\n");
+        return 22;
+    }
+    // initialize variables
+    int start = atoi(argv[1]);
+    int interest = atoi(argv[2]);
+
+    // debugging purposes
+    
+    /* 
+    printf("%i, %i", argc, argv[0]);
+    printf("start = %i, interest = %i", start, interest);
+    */
+
+    return 0;
+}
